ba79b27b8264757d1fec2b81b7d928d44035c70b
Shubham Rawat <rshubham589@gmail.com>
2021-11-22 02:14:01 +0530

    Website assets added homepagesetup Version 1.1.1

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  // SLick SLiders js
+  $(".sliders").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  // SLick SLiders js Ends
+
+  //document Ready Ends
+});
