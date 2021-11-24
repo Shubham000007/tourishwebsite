ed0a3ce23489b01e0e82f549f6ba23a41af86c68
Shubham Rawat <rshubham589@gmail.com>
2021-11-24 15:13:07 +0530

    Package Information Structure created Version 3.2.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,15 @@ $(document).ready(function () {
     prevArrow: '<div class="caret-left"><i class="fas fa-arrow-left"></i></div>',
     nextArrow: '<div class="caret-right"><i class="fas fa-arrow-right"></i></div>'
   });
+
+  $(".pack_sliders").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<div class="caret-left"><i class="fas fa-arrow-left"></i></div>',
+    nextArrow: '<div class="caret-right"><i class="fas fa-arrow-right"></i></div>'
+  });
   // SLick SLiders js Ends
 
   document.getElementById('banner__video').play();
